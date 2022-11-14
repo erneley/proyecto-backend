7f95a9557ccc7e96d0fc913e58ccc2c810ab2367
erneley <105618061+erneley@users.noreply.github.com>
2022-11-13 20:09:32 -0500

    Add files via upload

--- src/routes/proyecto.js
+++ src/routes/proyecto.js
@@ -2,43 +2,32 @@ const express=require("express");
 const proyectos = require("../models/proyecto");
 const router= express.Router();
 
-
-                    
-
 router.post("/", async (req,res)=>{
 
-
-    const validarproyecto= await proyecto.findOne({ numero: req.body.numero});
+    const validarproyecto=  await proyectos.findOne({ numero: req.body.numero});
     if (validarproyecto){
-        return res.send("ya existe un proyecto con ese numero")
+        return res.send("ya existe un proyecto con ese numero");
     }
     
-
-    
-    let proyecto = new proyectos();
+      
+  let proyecto = new proyectos();
    proyecto.numero= req.body.numero;
     proyecto.titulo= req.body.titulo;
     proyecto.fecha_inicio= req.body.fecha_inicio;
     proyecto.fecha_entrega= req.body.fecha_entrega;
     proyecto.valor= req.body.valor;
     proyecto.cliente= req.body.cliente._id;
-
     proyecto.tipo_proyecto= req.body.tipo_proyecto._id;
     proyecto.universidad= req.body.universidad._id;
     proyecto.etapa_proyecto= req.body.etapa_proyecto._id;
-    
-
+ 
     proyecto.fecha_creacion= new Date();
     proyecto.fecha_update= new Date();
-    
-    
-
+   
 proyecto= await proyecto.save()
     .then((data)=>res.send("los datos han sido guardados"))
     .catch((error)=>res.json({ message: error}))
-   
-
-
+  
 });
 
 
@@ -67,6 +56,11 @@ router.get("/", async (req,res)=>{
    
 
 });
+
+
+
+
+
 
 
 router.put("/:proyectoid", async (req,res)=>{
